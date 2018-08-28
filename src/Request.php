1520fbe01bfd6efb87cb45215b03150353e5d928
marcos <marcos.adamczuk@gmail.com>
2018-08-27 22:34:08 -0300

    added Request class

---
+++ src/Request.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Router;
+
+class Request {
+
+  private $path; // string
+  private $query = []; // [string => string]
+  private $method; // string
+  private $params = []; // [string => string]
+
+  public function __construct() {
+
+    $this->method = $_SERVER['REQUEST_METHOD'];
+    
+    $uri = $_SERVER['REQUEST_URI'];
+    $parsed_url = parse_url($uri);
+
+    $this->path = $parsed_url['path'];
+
+    if (isset($parsed_url['query'])) {
+      $query_parts = explode('&', $parsed_url['query']);
+      array_map(function($part) {
+        $pair = explode('=', $part);
+        $this->query[$pair[0]] = $pair[1];
+      }, $query_parts);
+    }
+  }
+
+  public function __get($name) {
+    return $this->$name;
+  }
+
+  public function addParam(string $name, string $value) {
+    $params[$name] = $value;
+  }
+}
